e89a2e16a372b1f587aad368876bb3152c633f72
Enza Rafaela De Nadai Victal Mariano <rafaeladenadai@gmail.com>
2023-08-07 12:10:59 -0300

    fix: ajusta descricao link agenda

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -234,7 +234,7 @@ const mural = [
     {
         title: "Agenda para o dia 1.1",
         text: "Confira aqui todo detalhamento do dia 1.1 que ocorre nesta segunda-feira, dia 07/08",
-        linksTitle: ["Mais informações aqui"],
+        linksTitle: ["Link para agenda do dia"],
         links: ["https://trybecourse.slack.com/archives/C05KM4D7L82/p1691195400640379"]
     },
     {
